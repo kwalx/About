d2df991de4c93d56673e3922b4002ae3f0b32373
alexanderKw <alkrivulya@gmail.com>
2017-05-05 03:57:14 +0300

    js file

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,32 @@ var skills = document.querySelectorAll('#skills-box .skill');
 var navList = document.querySelector('#nav-list');
 
 navList.addEventListener('click', function (e) {
-  e.preventDefault();
+  if (e.target.parentNode.classList != 'active') {
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = navList.children[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var item = _step.value;
+
+        item.classList.remove('active');
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+
+    e.target.parentNode.classList.add('active');
+  }
 });
-console.log(navList);
--- appIn/jsIn/scriptIn.js
+++ appIn/jsIn/scriptIn.js
@@ -6,6 +6,10 @@ const skills = document.querySelectorAll('#skills-box .skill');
 const navList = document.querySelector('#nav-list');
 
 navList.addEventListener('click', e => {
-  e.preventDefault();
+  if (e.target.parentNode.classList != 'active') {
+    for (let item of navList.children) {
+      item.classList.remove('active');
+    }
+    e.target.parentNode.classList.add('active');
+  }
 });
-console.log(navList);
